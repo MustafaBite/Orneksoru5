25be73e95607d0f47c48fa45299dea308359c4fc
Mustafa Bite <mustafabite2357@gmail.com>
2025-01-03 21:18:03 +0300

    Initial commit

---
+++ src/orneksoru5/Orneksoru5.java
@@ -0,0 +1,15 @@
+package orneksoru5;
+public class Orneksoru5 {
+    public static void main(String[] args) {  
+      for (int s=1;s<10;s++) {
+            if (s != 6) 
+            {        
+          System.out.print(s+"/"+(s+1)); 
+          if (s < 9){
+              System.out.print(",");
+          }
+            }        
+      }
+    }
+    }
+
